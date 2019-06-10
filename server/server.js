42317356b02bd17ea7a9bf40930f996a7ec55805
tamalera <florence.meier@alumni.ethz.ch>
2019-06-10 11:49:56 +0200

    Add input sanitisation and eslint

--- server/server.js
+++ server/server.js
@@ -1,29 +1,28 @@
-//npm modules
+// npm modules
 const express = require('express');
-const uuid = require('uuid/v4')
-const session = require('express-session')
+const uuid = require('uuid/v4');
+const session = require('express-session');
 const FileStore = require('session-file-store')(session);
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const path = require('path');
 const mysql = require('mysql');
 const bcrypt = require('bcrypt');
+const { check } = require('express-validator/check');
 
 // Needed for encryption of PW
 const saltRounds = 10;
 
 // TODO: Get mysql connection in different file
 const connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'root',
-  database : 'dev_DB'
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'dev_DB'
 });
 
-let options = {
-  genid: (req) => {
-    return uuid() // use UUIDs for session IDs
-  },
+const options = {
+  genid: req => uuid(), // use UUIDs for session IDs
   store: new FileStore(),
   secret: 'keyboard cat',
   resave: false,
@@ -35,9 +34,9 @@ let options = {
 const app = express();
 
 // add & configure middleware
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(session(options))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(session(options));
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -50,27 +49,27 @@ app.get('/', (req, res) => {
     connection.query("UPDATE testUsers SET password = hash WHERE email='test@test.com';");
   });
   */
-  if(req.session){
-    res.sendFile(path.join(__dirname + '/login.html'));
+  if (req.session) {
+    res.sendFile(path.join(`${__dirname}/login.html`));
   }
-})
+});
 
 app.get('/auth', (req, res) => {
-  if(req.session){
-    if(req.session.loggedin){
-      res.sendFile(path.join(__dirname + '/memberZone.html'));
+  if (req.session) {
+    if (req.session.loggedin) {
+      res.sendFile(path.join(`${__dirname}/memberZone.html`));
     }
     else {
-      console.log("User: " + req.session.email + " wanted to get here.");
-      res.sendFile(path.join(__dirname + '/errorPage.html'));
+      console.log(`User: ${req.session.email} wanted to get here.`);
+      res.sendFile(path.join(`${__dirname}/errorPage.html`));
     }
   }
-})
+});
 
 app.get('/logout', (req, res) => {
   if (req.session) {
     if (req.session.email) {
-      console.log("User: " + req.session.email + " logged out.");
+      console.log(`User: ${req.session.email} logged out.`);
       // undo session
       req.session.loggedin = false;
       req.session.email = '';
@@ -78,50 +77,59 @@ app.get('/logout', (req, res) => {
       req.logout();
       res.clearCookie(options.name);
       res.redirect('/');
-    } else {
-        console.log("Unknown user wanted to log out");
-        res.sendFile(path.join(__dirname + '/errorPage.html'));
+    }
+    else {
+      console.log('Unknown user wanted to log out');
+      res.sendFile(path.join(`${__dirname}/errorPage.html`));
     }
   }
-})
+});
 
 
-app.post('/login', (request, response) => {
-  let username = request.body.email;
-  let password = request.body.password;
+app.post('/login', [
+  check('email').isEmail().trim().escape()
+    .stripLow(),
+  check('password').trim().escape().stripLow()
+],
+(request, response) => {
+  const username = request.body.email;
+  const { password } = request.body;
 
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  const emailOk = re.test(String(username).toLowerCase())
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const emailOk = re.test(String(username).toLowerCase());
 
   if (username && emailOk) {
-    let sql = "SELECT * FROM ?? WHERE ?? = ?";
+    let sql = 'SELECT * FROM ?? WHERE ?? = ?';
     const inserts = ['testUsers', 'email', username];
     sql = mysql.format(sql, inserts);
-    connection.query(sql, function(error, results, fields) {
+    connection.query(sql, (error, results) => {
       if (results.length > 0) {
         if (bcrypt.compareSync(password, results[0].password)) {
           request.session.loggedin = true;
           request.session.email = username;
-          console.log("User: " + username + " logged in.");
+          console.log(`User: ${username} logged in.`);
           response.redirect('/auth');
-        } else {
-        console.log("User: " + username + " used wrong credentials.");
+        }
+        else {
+          console.log(`User: ${username} used wrong credentials.`);
+          response.send('Incorrect Username and/or Password!');
+        }
+      }
+      else {
+        console.log(`No user found in DB with: ${username}`);
         response.send('Incorrect Username and/or Password!');
       }
-      } else {
-        console.log("No user found in DB with: " + username);
-        response.send('Incorrect Username and/or Password!');
-      }     
       response.end();
     });
-  } else {
-    console.log("Incomplete credentials used");
+  }
+  else {
+    console.log('Incomplete credentials used');
     response.send('Please enter Username and Password!');
     response.end();
   }
-})
+});
 
 // tell the server what port to listen on
 app.listen(3000, () => {
-  console.log('Listening on localhost:3000')
-})
+  console.log('Listening on localhost:3000');
+});
